72d4a336363eb90a4b842089fdc81b4143f7690d
Emily Su <emilyzqsu@gmail.com>
2024-02-14 14:11:23 -0500

    Updated the UI and added comments in renderer.js

--- src/scripts/renderer.js
+++ src/scripts/renderer.js
@@ -8,9 +8,11 @@ checkbox = document.querySelector('.checkbox');
 
 checkbox.addEventListener('change', function engorNot(){   
     if(checkbox.checked) {
+        // Don't display "Transcription + Translation" if input file is in English
         document.querySelector('.process #no-eng').style.display = "none";
     }
     else {
+        // Display "Transcription + Translation" if input file is not English 
         document.querySelector('.process #no-eng').style.display = "block";
     }
 }
