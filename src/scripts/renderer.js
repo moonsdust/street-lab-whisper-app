c53d237efb5e53d91e6456aa85992172041315ef
Emily Su <emilyzqsu@gmail.com>
2024-02-14 13:02:50 -0500

    Updated UI

---
+++ src/scripts/renderer.js
@@ -0,0 +1,17 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process unless
+// nodeIntegration is set to true in webPreferences.
+// Use preload.js to selectively enable features
+// needed in the renderer process.
+checkbox = document.querySelector('.checkbox');
+
+checkbox.addEventListener('change', function engorNot(){   
+    if(checkbox.checked) {
+        document.querySelector('.process #no-eng').style.display = "none";
+    }
+    else {
+        document.querySelector('.process #no-eng').style.display = "block";
+    }
+}
+);
